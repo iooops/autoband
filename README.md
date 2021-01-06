0e40842b2386aec5d65bc7776288aa5b5e9e7525
一只追逐蜻蜓的小猫猫 <digdongaa@gmail.com>
2021-01-06 16:14:31 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+
+# autoband
+
+# RUN
+`pip3 install -r requirements.txt -t ./modules`
+
+`flask run`
+
+# Add python dependencies
+add lines in requirements.txt
+
+
---
+++ app.py
@@ -0,0 +1,29 @@
+
+import sys
+sys.path.insert(0, "./modules")
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+   return "Hello Flask"
+
+@app.route("/users")
+def users():
+   users = [{'name': 'test1'}, {'name': 'test2'}]
+   return jsonify(data=users)
+
+@app.route("/users/<id>")
+def user(id):
+   return jsonify(data={'name': 'test1'})
+
+@app.route("/gen_drum_groove", methods=['POST'])
+def gen_drum_groove():
+	req = request.get_json()
+	tap_sounds = req.get('tap_sounds')
+	# TODO: generate drum groove here
+	return 'generated drum groove here'
+
+
